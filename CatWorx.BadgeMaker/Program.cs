b26527d83df41caeb3e49536d481616f8d2174c0
Jsheen98 <jsheendev@gmail.com>
2023-06-29 19:21:26 -0600

    Added Employee class and methods

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -5,16 +5,51 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<Employee> GetEmployees()
         {
-            List<string> employees = new List<string>() { "adam", "amy" };
-            employees.Add("barbara");
-            employees.Add("billy");
-            
+            List<Employee> employees = new List<Employee>();
+            // Collect user values until the value is an empty string
+            while (true)
+            {
+                // Move the initial prompt inside the loop, so it repeasts for each employee
+                Console.WriteLine("Enter first name: (leave empty to exit): ");
+
+                string firstName = Console.ReadLine() ?? "";
+                if (firstName == "")
+                {
+                    break;
+                }
+
+                //  add Console.ReadLine() for each value
+                Console.Write("Enter a last name: ");
+                string lastName = Console.ReadLine() ?? "";
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+                Console.Write("Enter Photo URL: ");
+                string photoUrl = Console.ReadLine() ?? "";
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                // Add currentEmployee, not a string
+                employees.Add(currentEmployee);
+            }
+            return employees;
+        }
+
+
+        static void PrintEmployees(List<Employee> employees)
+        {
             for (int i = 0; i < employees.Count; i++)
             {
-                Console.WriteLine(employees[i]);
+                // each item in employees is now an Employee instance
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
             }
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<Employee> employees = GetEmployees();
+            PrintEmployees(employees);
         }
     }
 }
